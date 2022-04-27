85e84bf7d6685841b2a5c06cd6a05f9f3e0df465
sumeragizzz <developer@sumeragizzz.dev>
2022-04-27 22:01:14 +0900

    Spring Initializrにてプロジェクト作成

---
+++ src/main/java/dev/sumeragizzz/golbanker/GoLBankerApplication.java
@@ -0,0 +1,13 @@
+package dev.sumeragizzz.golbanker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoLBankerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoLBankerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sumeragizzz/golbanker/GoLBankerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sumeragizzz.golbanker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoLBankerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
